511b0e7e727ad35215a72a9e00971d69831dc332
apple <>
2019-04-12 08:51:03 +0800

    V1.0

---
+++ MyGit/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MyGit
+//
+//  Created by apple on 2019/4/12.
+//  Copyright © 2019 com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MyGit/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MyGit
+//
+//  Created by apple on 2019/4/12.
+//  Copyright © 2019 com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
